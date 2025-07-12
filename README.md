d25372067cbe9e537f97bcb9a25e8bb392f41324
Kyeong Soo Choi <mandoo180@gmail.com>
2025-07-13 03:01:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Neovim Config
+
+Stolen from [nvim-lite](https://github.com/radleylewis/nvim-lite)
---
+++ lua/plugins/init.lua
@@ -0,0 +1,26 @@
+return {
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+          -- load the colorscheme here
+          vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+
+    {
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.8', -- or, branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        end,
+    }
+    
+}
